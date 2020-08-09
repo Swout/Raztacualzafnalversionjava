ff0910cb45ad8fb870d89c724bb50d30ef5947e6
Oscar Portillo <66044741+Swout@users.noreply.github.com>
2020-08-09 15:34:31 -0600

    Creacion de la aplicacion con sus implentaciones de libreria, y vinculacion con firebase realizada

---
+++ app/src/main/java/com/swout/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.swout.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Rastacualza");
+    }
+
+
+
+}
