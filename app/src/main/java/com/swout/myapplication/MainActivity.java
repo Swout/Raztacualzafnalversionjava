650ca78ce17bdd7abfbe1833598eb00e861709df
Oscar Portillo <66044741+Swout@users.noreply.github.com>
2020-08-14 09:04:29 -0600

    resolucion problema del design

--- app/src/main/java/com/swout/myapplication/MainActivity.java
+++ app/src/main/java/com/swout/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package com.swout.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
