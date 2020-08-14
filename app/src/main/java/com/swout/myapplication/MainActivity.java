c6ea66333566abb1276458a375d3b8cd0adfd252
Oscar Portillo <66044741+Swout@users.noreply.github.com>
2020-08-14 11:44:07 -0600

    implementancion de firebase primera fase

--- app/src/main/java/com/swout/myapplication/MainActivity.java
+++ app/src/main/java/com/swout/myapplication/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager myViewPager;
     private TabLayout myTableLayout;
     private TabsAccessorAdapter myTabsAccessorAdapter;
+    private FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,17 @@ public class MainActivity extends AppCompatActivity {
         myTableLayout.setupWithViewPager(myViewPager);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (currentUser == null)
+        {
+            SendUserToLoginActivity();
+        }
+    }
 
-
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(loginIntent);
+    }
 }
