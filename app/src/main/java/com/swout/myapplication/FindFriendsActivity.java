653efd341127f3cb79c1311f76d8eb8864fbeec2
Oscar Portillo <66044741+Swout@users.noreply.github.com>
2020-08-18 17:41:32 -0600

    Contactos status y estado en linea

--- app/src/main/java/com/swout/myapplication/FindFriendsActivity.java
+++ app/src/main/java/com/swout/myapplication/FindFriendsActivity.java
@@ -1,14 +1,28 @@
 package com.swout.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class FindFriendsActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private RecyclerView FindFriendsRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
+
+        FindFriendsRecyclerView = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mToolbar = (Toolbar) findViewById(R.id.find_friends_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Find Friends");
     }
 }
--- app/src/main/java/com/swout/myapplication/MainActivity.java
+++ app/src/main/java/com/swout/myapplication/MainActivity.java
@@ -121,6 +121,8 @@ public class MainActivity extends AppCompatActivity {
         }
         if(item.getItemId() == R.id.main_find_friends_option){
 
+            SendUserToFindFriendsActivity();
+
         }
         return true;
     }
@@ -177,5 +179,11 @@ public class MainActivity extends AppCompatActivity {
         settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingsIntent);
         finish();
+    }
+
+    private void SendUserToFindFriendsActivity() {
+        Intent findFriendIntent = new Intent(MainActivity.this,FindFriendsActivity.class);
+        startActivity(findFriendIntent);
+
     }
 }
