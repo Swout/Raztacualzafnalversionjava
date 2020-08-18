037952c63cb05c8bb14be69a772e0b920aa508b0
Oscar Portillo <66044741+Swout@users.noreply.github.com>
2020-08-18 16:51:37 -0600

    Arreglado subir la foto de perfil en el firebase

--- app/src/main/java/com/swout/myapplication/SettingsActivity.java
+++ app/src/main/java/com/swout/myapplication/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.swout.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -11,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,8 +23,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,6 +43,10 @@ public class SettingsActivity extends AppCompatActivity {
     private String currentUserId;
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
+    private StorageReference UserProfileImageRef;
+    private ProgressDialog loadingBar;
+
+    private static final int GalleryPick = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +56,8 @@ public class SettingsActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         currentUserId = mAuth.getCurrentUser().getUid();
         RootRef = FirebaseDatabase.getInstance().getReference();
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("Profile Image/");
+
 
         InitializeFields();
 
@@ -54,6 +71,26 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
         RetrieveUserInfo();
+
+        userProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+
+
+                    CropImage.activity()
+                            .setGuidelines(CropImageView.Guidelines.ON)
+                            .setAspectRatio(1,1)
+                            .start(SettingsActivity.this);
+
+
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,GalleryPick);
+            }
+        });
     }
 
     private void RetrieveUserInfo() {
@@ -70,6 +107,8 @@ public class SettingsActivity extends AppCompatActivity {
 
                             userName.setText(retriveUserName);
                             userStatus.setText(retriveStatus);
+                            Picasso.get().load(retriveProfileImage).into(userProfileImage);
+
 
 
                         }
@@ -135,5 +174,74 @@ public class SettingsActivity extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.set_user_name);
         userStatus = (EditText) findViewById(R.id.set_profile_status);
         userProfileImage = (CircleImageView) findViewById(R.id.profile_image);
+        loadingBar = new ProgressDialog(this);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if(resultCode == RESULT_OK){
+
+                loadingBar.setTitle("Set profile image");
+                loadingBar.setMessage("Image profile is updating");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+                Uri resultUri=result.getUri();//This contains the cropped image
+
+                final StorageReference filePath=UserProfileImageRef.child(currentUserId+".jpg");//This way we link the userId with image. This is the file name of the image stored in firebase database.
+
+                UploadTask uploadTask=filePath.putFile(resultUri);
+                Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+
+                        // Continue with the task to get the download URL
+                        return filePath.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            Uri downloadUri = task.getResult();
+                            Toast.makeText(SettingsActivity.this, "Successfully uploaded", Toast.LENGTH_SHORT).show();
+                            if (downloadUri != null) {
+
+                                String downloadUrl = downloadUri.toString(); //YOU WILL GET THE DOWNLOAD URL HERE !!!!
+                                RootRef.child("Users").child(currentUserId).child("image").setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        loadingBar.dismiss();
+                                        if(!task.isSuccessful()){
+                                            String error=task.getException().toString();
+                                            Toast.makeText(SettingsActivity.this,"Error : "+error,Toast.LENGTH_LONG).show();
+                                        }else{
+
+                                        }
+                                    }
+                                });
+                            }
+
+                        } else {
+                            // Handle failures
+                            // ...
+                            Toast.makeText(SettingsActivity.this,"Error",Toast.LENGTH_LONG).show();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+            }
+        }
+
+
     }
 }
